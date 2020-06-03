30ead568d4345b5df81004ba48fb0a532921cc34
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2020-06-03 23:55:25 +1200

    Add app.js & tag info

---
+++ src/app.js
@@ -0,0 +1,18 @@
+var tags = {
+    "code": [
+        "index.html"
+    ],
+    "deliverable": null,
+    "asset": [
+        "usageTesting",
+        "design-sketch.jpg"
+    ],
+    "in-progress": [
+        "index.html",
+        "se350A3"
+    ],
+    "needs-review": null,
+    "done": [
+        "usageTesting"
+    ]
+}
